e3bdd73974bea2c3479b28bc6a039fdf3c7aac4c
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2020-10-30 08:33:19 +0100

    Fixed download error

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/download", methods=["POST", "GET"])
+def download(filename):
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    # Returning file from appended path
+    return send_from_directory(directory=uploads, filename=transpha.exe)
 
-@app.route("/download", methods=["POST"])
-def download_file():
-    try:
-        res= make_response(send_file('file/transpha.exe', 
-                        mimetype='application/x-msdownload'))
-        res.headers['Content-Disposition'] = 'attachment; filename=transpha.exe'
+# @app.route("/download", methods=["POST"])
+# def download_file():
+#     try:
+#         res= make_response(send_file('file/transpha.exe', 
+#                         mimetype='application/x-msdownload'))
+#         res.headers['Content-Disposition'] = 'attachment; filename=transpha.exe'
 
-    except Exception as e:
-        res= jsonify({'error': "Oops! Try again."+str(e)})
-        res.status_code = 500
-    return res
-
+#     except Exception as e:
+#         res= jsonify({'error': "Oops! Try again."+str(e)})
+#         res.status_code = 500
+#     return res
